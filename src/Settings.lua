3f73650bb6ba3a390ef69fefcfb9cec59fe317f0
NameTakenBonk <rvitus17@gmail.com>
2025-08-09 16:43:06 +0100

    Fixed settings

--- src/Settings.lua
+++ src/Settings.lua
@@ -4,11 +4,11 @@ return {
         [Enum.Material.Plastic] = 10
     }, --> this is here just for the future for richochet and penetration
 
-    Visualise = true, --> basically debug
-	SafeMode = false, --> if to let the server handle the bullet replication or not
+    Visualise = false, --> basically debug
+	SafeMode = true, --> if to let the server handle the bullet replication or not
 	ParallelProcessing = true, --> if should use multiple threads or not
 	
-	LoadBalanceStrategy = "RoundBin", --> "RoundBin", "LeastLoadedActor"
+	LoadBalanceStrategy = "LeastLoadedActor", --> "RoundBin", "LeastLoadedActor"
 	UpdateRate = 30, --> 30hz as default
 	ActorAmount = 12,
 }
